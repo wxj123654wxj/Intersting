1012c70769d49312db98319a977ccaecc021b4e7
Administrator <666@666.com>
2020-10-30 15:21:37 +0800

    qq

--- MasterWang/src/New.java
+++ MasterWang/src/New.java
@@ -2,8 +2,8 @@ import java.io.File;
 
 public class New {
     public static void main(String args[]){
-        System.out.print("jjx sb");
-        System.out.print("jjx dsb");
+        System.out.print("wxj sb");
+        System.out.print("wxj dsb");
         File f = new File("\\xxx.txt");
         System.out.print("HelloWorld");
 }
