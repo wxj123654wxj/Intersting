bafc7964d558653fb4d8ee4128159a93fbe78124
Administrator <666@666.com>
2020-10-30 15:12:05 +0800

    qq

--- untitled/src/New.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-
-public class New {
-    public static void main(String args[]){
-        System.out.print("wxj sb");
-        System.out.print("wxj dsb");
-        File f = new File("\\xxx.txt");
-    }
-}
