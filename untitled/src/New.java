b3ceb68572d5caf86f4aaed1ea3771083fb5ecf2
Administrator <666@666.com>
2020-10-30 14:57:43 +0800

    jjx

--- untitled/src/New.java
+++ untitled/src/New.java
@@ -1,6 +1,6 @@
 public class New {
     public static void main(String args[]){
-        System.out.print("jjx sb");
-        System.out.print("jjx dsb");
+        System.out.print("wxj sb");
+        System.out.print("wxj dsb");
     }
 }
