549d8a167273c6b05ad0058e3548e9f4613f8119
Administrator <666@666.com>
2020-10-30 15:42:33 +0800

    qq

---
+++ MasterWang/src/Main.java
@@ -0,0 +1,13 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[]args) throws ClassNotFoundException, SQLException {
+        System.out.println("Hello World");
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection  = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","");
+        PreparedStatement preparedStatement = connection.prepareStatement("select * form db");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        System.out.println(resultSet);
+
+    }
+}
