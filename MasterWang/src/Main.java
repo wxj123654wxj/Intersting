524fd7dca7d23b3c9cf0408a95703a031ec00bbc
Administrator <666@666.com>
2020-10-30 16:01:26 +0800

    qq

--- MasterWang/src/Main.java
+++ MasterWang/src/Main.java
@@ -3,11 +3,15 @@ import java.sql.*;
 public class Main {
     public static void main(String[]args) throws ClassNotFoundException, SQLException {
         System.out.println("Hello World");
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection  = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","");
-        PreparedStatement preparedStatement = connection.prepareStatement("select * form db");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection  = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?serverTimezone =UTC","root","");
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from inf_student");
         ResultSet resultSet = preparedStatement.executeQuery();
-        System.out.println(resultSet);
+        while (resultSet.next()){
+            System.out.print(resultSet.getInt(1)+"\t");
+            System.out.print(resultSet.getString(2)+"\t");
+            System.out.print(resultSet.getString(3));
+        }
 
     }
 }
