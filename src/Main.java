be779ff05a3866947d664df4a5d2067018b55695
Administrator <666@666.com>
2020-10-30 15:31:48 +0800

    aa

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[]args) throws ClassNotFoundException, SQLException {
+        System.out.println("Hello World");
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection  = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","");
+        PreparedStatement preparedStatement = connection.prepareStatement("select * form db");
+        ResultSet resultSet = preparedStatement.getResultSet();
+        System.out.println(resultSet);
+    }
+}
